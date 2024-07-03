317355d7e54d9bc2cfaf2988b8d91a9014132654
Jarek <gwizdala.jarek@gmail.com>
2024-07-03 17:23:34 +0200

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatCardModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  
+  items = signal<Block[]>([
+    {title:'Title1',text:'testText',created:new Date()}
+  ]);
+}
+
+type Block = {
+  title: string;
+  text:string;
+  created: Date;
 }
