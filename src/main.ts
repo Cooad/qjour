f22f3338487ff455b06a4d547e8abf4820ffa23a
Jarek <gwizdala.jarek@gmail.com>
2024-07-10 23:37:41 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { APP_SETTINGS, AppSettings } from './appsettings';
-import { APP_INITIALIZER } from '@angular/core';
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
 import { initDatabase } from './app/services/database.service';
 
 fetch('appsettings.json')
   .then<Response, AppSettings>(response => response.json())
   .catch<AppSettings>(error => ({}))
-  .then(settings => bootstrapApplication(AppComponent, appConfig([
-    {
-      provide: APP_SETTINGS,
-      useValue: settings
-    },
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      deps: [APP_SETTINGS],
-      useFactory: (settings: AppSettings) => () => initDatabase(settings)
-    }
-  ])))
+  .then(settings => bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(routes),
+      provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      }),
+      {
+        provide: APP_SETTINGS,
+        useValue: settings
+      },
+      {
+        provide: APP_INITIALIZER,
+        multi: true,
+        deps: [APP_SETTINGS],
+        useFactory: (settings: AppSettings) => () => initDatabase(settings)
+      }
+    ]
+  }))
   .catch((err) => console.error(err));
